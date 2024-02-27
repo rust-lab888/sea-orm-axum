875d6e222876d977fd3e779b52747f5afc86faab
inadati <dr.inadati@gmail.com>
2024-02-28 08:01:42 +0900

    updateとdeleteを追加 (#3)

--- src/presentation/app.rs
+++ src/presentation/app.rs
@@ -1,6 +1,13 @@
 use sea_orm::DatabaseConnection;
+use serde::{Serialize, Deserialize};
 
 #[derive(Clone)]
 pub struct State {
     pub conn: DatabaseConnection,
 }
+
+#[derive(Serialize, Deserialize, Debug)]
+pub struct DeleteResult {
+    pub message: String,
+}
+
--- src/usecase/character.rs
+++ src/usecase/character.rs
@@ -7,8 +7,7 @@ pub async fn get_all<R>(repo: &R) -> anyhow::Result<Vec<character::Model>>
 where
     R: CharacterRepository,
 {
-    let characters = repo.get_characters().await?;
-    Ok(characters)
+    Ok(repo.get_characters().await?)
 }
 
 pub async fn create<R>(repo: &R, name: &str) -> anyhow::Result<character::Model>
@@ -16,4 +15,19 @@ where
     R: CharacterRepository,
 {
     Ok(repo.create_character(name).await?)
+}
+
+pub async fn update<R>(repo: &R, id: i32, name: &str) -> anyhow::Result<character::Model>
+where
+    R: CharacterRepository,
+{
+    Ok(repo.update_character(id, name).await?)
+}
+
+pub async fn delete<R>(repo: &R, id: i32) -> anyhow::Result<()>
+where
+    R: CharacterRepository,
+{
+    repo.delete_character(id).await?;
+    Ok(())
 }
