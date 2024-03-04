414a0640850e27c7289926c3f822966d9343b4de
inadati <dr.inadati@gmail.com>
2024-03-04 13:28:44 +0900

    カンマは要らないので削除 (#5)

--- src/usecase/character.rs
+++ src/usecase/character.rs
@@ -5,28 +5,28 @@ use crate::domain::repository::character::CharacterRepository;
 
 pub async fn get_all<R>(repo: &R) -> anyhow::Result<Vec<character::Model>>
 where
-    R: CharacterRepository,
+    R: CharacterRepository
 {
     Ok(repo.get_characters().await?)
 }
 
 pub async fn create<R>(repo: &R, name: &str) -> anyhow::Result<character::Model>
 where
-    R: CharacterRepository,
+    R: CharacterRepository
 {
     Ok(repo.create_character(name).await?)
 }
 
 pub async fn update<R>(repo: &R, id: i32, name: &str) -> anyhow::Result<character::Model>
 where
-    R: CharacterRepository,
+    R: CharacterRepository
 {
     Ok(repo.update_character(id, name).await?)
 }
 
 pub async fn delete<R>(repo: &R, id: i32) -> anyhow::Result<()>
 where
-    R: CharacterRepository,
+    R: CharacterRepository
 {
     repo.delete_character(id).await?;
     Ok(())
